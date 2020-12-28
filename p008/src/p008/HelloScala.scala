e892c35855ec43cab645dabab215324035083f23
sepiggy <jms1209@qq.com>
2020-12-29 00:19:59 +0800

    B4 p011 ok

---
+++ p008/src/p008/HelloScala.scala
@@ -0,0 +1,8 @@
+package p008
+
+// object 是伴生对象
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello, scala!")
+  }
+}
---
+++ p009/src/main/scala/cn/sepiggy/TestScala.scala
@@ -0,0 +1,9 @@
+package cn.sepiggy
+
+// 1. 对应于TestScala$类的一个静态对象MODULE$
+// 2. 单例
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello, scala by IDEA...")
+  }
+}
